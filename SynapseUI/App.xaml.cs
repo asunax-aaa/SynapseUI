b3d5f4fd02fdc2d9988dbf79902c1f02532bfc12
asunax <61892568+asunax-aaa@users.noreply.github.com>
2021-09-05 18:59:53 +0100

    Updater fix

--- SynapseUI/App.xaml.cs
+++ SynapseUI/App.xaml.cs
@@ -186,7 +186,6 @@ namespace SynapseUI
             downloader.Add(new FileEntry("Updater.exe"));
 
             VersionChecker.Run(downloader);
-            App.Debug(downloader.ToString());
 
             downloader.Begin();
 
